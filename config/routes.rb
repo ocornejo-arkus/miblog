b7ae8a7323a724b493ef934b2b17f1ac7883104b
ocornejo <ocornejo@arkusnexus.com>
2021-04-16 11:34:03 -0500

    Páginas estaticas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'estaticas/contacto'
+  get 'estaticas/nosotros'
+  root 'home#index'
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
