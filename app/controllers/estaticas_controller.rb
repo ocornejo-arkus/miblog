50ff3236183a42d7d37ed6cb3b46276bc0d62ce2
ocornejo <ocornejo@arkusnexus.com>
2021-04-16 13:09:35 -0500

    Páginas estáticas

--- app/controllers/estaticas_controller.rb
+++ app/controllers/estaticas_controller.rb
@@ -1,7 +1,11 @@
 class EstaticasController < ApplicationController
   def contacto
+    @id=params[:id].to_i
+    @pagina=params[:pagina]
   end
 
   def nosotros
+    @mensaje = "Hola desde el controller"
+    @usuarios = ['Miguel', 'Luis', 'Josué', 'Noé']
   end
 end
